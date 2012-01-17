ae4f75fad9ca24ba74193b1bc40041edb9916391
Kangkook Jee <aixer77@gmail.com>
2012-01-17 16:01:22 -0500

    added smount.sh

---
+++ etc/smount.sh
@@ -0,0 +1,22 @@
+#mkdir /Volumes/shome
+
+error_msg () {
+    echo $1
+    exit -1
+}
+
+if [ -z  "$1" ]
+then
+    ADDR=sos14.cs.columbia.edu
+else
+    ADDR=$1
+fi
+
+echo "Connecting to " $ADDR
+
+for IMG in tfa++
+do
+   echo "mounting ${IMG}"
+   mkdir /Volumes/${IMG}
+   sshfs jikk@${ADDR}:/home/jikk/${IMG} /Volumes/${IMG} -oauto_cache,reconnect,volname=${IMG} || error_msg "can't mount ${IMG}"
+done
